db1abb83bdbffdacce0fc22d2ffba6ebdd07e32d
Jasmine C <jasminecheng@live.com>
2021-02-20 00:08:40 -0500

    Updated game package and added boards

--- README.md
+++ README.md
@@ -1,16 +1,41 @@
 # AWAP 2021 Starter Code
 
-## Requirements
+## Getting Started
 You will need Python 3.
 
-Python packages (can be installed via `pip`):
-- numpy
-- pyyaml
-- tkinter
-- pandas
+From within the folder run
+```
+pip install -r requirements.txt
+```
+to install all the dependencies.
 
-Game package:
-From within the `awap2021-public` folder, run,
+Run
 ```
-pip install dist/awap-2021-0.0.1.tar.gz    
+python main.py
 ```
+to execute the game
+
+## Data Structures
+
+## Feedback object
+
+```
+'wait_time': Time from passenger spawn to boarding the train
+'travel_time': Time spent traveling from source to destination
+'total_time': wait_time + travel_time
+'num_intermediate_stations': Number of stations passenger goes 
+through before reaching destination (non-inclusive of start and stop station)
+'travel_distance': Distance passenger travels as it traverses from
+start to stop station along train route, i.e., a sum of the lengths
+of each segment between stations along the passenger's route.
+'displacement': Euclidean distance between start and end stations 
+```
+
+
+## Tips 
+Comment out the line 
+```
+g.visualize()
+```
+to prevent the visualization window from popping up.
+
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from game.game import Game
-
-
-MAX_ATTEMPTS = 4
-MAX_TRAINS = 4
+import time
 
 def example_network_generator(station_locations, feedback):
     network = {
-            0: [0, 1],
-            1: [1, 2, 3, 4]
+        0: [0, 1, 2],
+        1: [2, 3, 4, 2]
     }
+
     return network
 
 if __name__ == '__main__':
     # Load the board file with station locations/types
-    g = Game('./board/board1.yaml')
+    g = Game('./board/board6.yaml')
+    SIM_ATTEMPTS = g.getGameConstants('SIM_ATTEMPTS')
+    MAX_TRAINS = g.getGameConstants('MAX_TRAINS')
 
     """
     Get a dictionary that maps a location (x, y) to a tuple (stationID, shapeID),
@@ -22,24 +22,33 @@ if __name__ == '__main__':
     station_locations = g.getStationLocations()
     feedback = None
 
-    for i in range(MAX_ATTEMPTS):
+    for i in range(SIM_ATTEMPTS):
+        tic = time.perf_counter() # Start a timer
         """
         Write your code here to take create a network! 
-        The network should be a dictionary that maps (trainColorID)->[stationID1, stationID2,
-        stationID3...]. 
+        The network should be a dictionary that maps (trainColorID)->[stationID1, 
+        stationID2, stationID3...]. 
         Routes have to be path or single cycle (no "lollipops or figure 8's etc). 
         Hint: you may want to pass `station_locations` and `feedback` from the 
         previous run into your function.
         """
         network = example_network_generator(station_locations, feedback)
+        
+        # Get the run time of your code
+        runtime = time.perf_counter() - tic 
+        if runtime > 10.0:
+            raise TimeoutError("Run time for your network generator cannot exceed 10 seconds.")
 
         """
-        Simulate commute using your network, and get back a feedback object. You can
-        query the object to get helpful stats like feedback.get_avg_wait_time(). 
+        Simulate commute using your network, and get back a feedback object. You 
+        can query the object to get helpful stats like feedback.get_avg_wait_time(). 
         """
         feedback = g.simulate(network)
-        
+        # Print your score from this run
+        feedback.print_score(runtime) 
+
         """
-        Display an animation of the simulation. 
+        Display an animation of the simulation. You can comment this out if you 
+        want to skip the animation
         """
-        g.visualize()
+        g.visualize(speed=10)
